e88c40146a368fd4510336f41e620f2f9f0cb03c
harshal239 <harshalwalunj49@gmail.com>
2022-06-12 19:49:08 +0530

    Updated BookList

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,9 @@ import { Title, BookList, AddBook } from "./components";
 import { createSignal, createEffect } from "solid-js";
 import { Show } from "solid-js";
 
-const initialBooks = [
-  { title: "Code Complete", author: "Steve McConnell" },
-  { title: "The Hobbit", author: "J.R.R. Tolkien" },
-];
-
 function App() {
   //similar to useState in react
-  const [books, setBooks] = createSignal(initialBooks);
+  const [books, setBooks] = createSignal([]);
   const [showBooks, setShowBooks] = createSignal(false);
 
   const toggleShowBooks = () => setShowBooks(!showBooks());
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,22 +1,40 @@
+import { Show } from "solid-js";
+
 //props should not be destructured
 const BookList = (props) => {
   //derived State
   const totalBooks = () => props.books.length;
   return (
     <>
-      <h1 className="text-lg font-semibold ">List of Books ({totalBooks()})</h1>
-      <ol className="list-decimal">
-        <For each={props.books}>
-          {(book) => {
-            return (
-              <li className="font-semibold">
-                {book.title}
-                <span className="font-normal italic">({book.author})</span>
-              </li>
-            );
-          }}
-        </For>
-      </ol>
+      <Show
+        when={totalBooks() > 0}
+        fallback={
+          <div className=" flex items-center gap-4">
+            No Books Found !{" "}
+            <img
+              src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/pensive-face_1f614.png"
+              alt="face"
+              className=" inline-block w-10 align-middle "
+            />
+          </div>
+        }
+      >
+        <h1 className="text-lg font-semibold ">
+          List of Books ({totalBooks()})
+        </h1>
+        <ol className="list-decimal">
+          <For each={props.books}>
+            {(book) => {
+              return (
+                <li className="font-semibold">
+                  {book.title}
+                  <span className="font-normal italic">({book.author})</span>
+                </li>
+              );
+            }}
+          </For>
+        </ol>
+      </Show>
       <hr className="w-full" />
     </>
   );
